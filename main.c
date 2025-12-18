df52779088a4099d7215f7277e10aac477cf42f4
Devlin76 <devlinmichnik123@gmail.com>
2025-12-18 13:12:16 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n1=0;
+    int n2=0;
+
+    printf("ingresa el primer numero:");
+    scanf("%d",&n1);
+    printf("ingrese el segundo numero");
+    scanf("%d",&n2);
+    printf("el resultado de la suma de las 2 variables es:%d",n1+n2);
+}
